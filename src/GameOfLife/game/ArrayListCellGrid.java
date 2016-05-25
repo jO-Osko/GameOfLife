e6d91a29141eb4a3d413b199d5e20c4875706c3a
Filip <koprivec.filip@gmail.com>
2016-05-25 17:25:44 +0200

    Implemented simple interfaces

---
+++ src/GameOfLife/game/ArrayListCellGrid.java
@@ -0,0 +1,53 @@
+package GameOfLife.game;
+
+
+import GameOfLife.Interfaces.CellGrid;
+import GameOfLife.Interfaces.UpdatableCellGrid;
+import GameOfLife.cell.CellState;
+
+import java.util.ArrayList;
+
+public class ArrayListCellGrid implements UpdatableCellGrid{
+
+    private ArrayList<ArrayList<CellState>> grid;
+    private int numberOfRows = 0;
+    private int numberOfColumns = 0;
+
+
+    public ArrayListCellGrid(){
+        this(0,0);
+    }
+
+    public ArrayListCellGrid(int numberOfRows, int numberOfColumns) {
+        this(numberOfRows, numberOfColumns, CellState.DEAD);
+    }
+
+    public ArrayListCellGrid(int numberOfRows, int numberOfColumns, CellState initialState){
+        this.numberOfRows = numberOfRows;
+        this.numberOfColumns = numberOfColumns;
+
+        this.grid = new ArrayList<>(numberOfRows);
+        for(int j = 0; j < numberOfRows; ++j){
+            ArrayList<CellState> row = new ArrayList<>(numberOfColumns);
+            for(int i = 0; i < numberOfColumns; ++i){
+                row.add(initialState);
+            }
+            this.grid.add(row);
+        }
+
+    }
+
+    @Override
+    public void updateGrid(ArrayList<ArrayList<CellState>> grid){
+        this.grid = grid;
+    }
+
+    @Override
+    public CellState getCellStateAt(int x_coor, int y_coor) {
+        if(x_coor < 0 || x_coor >= numberOfRows || y_coor < 0 || y_coor >= numberOfColumns){
+            // Automatically dead if outside of grid
+            return CellState.DEAD;
+        }
+        return this.grid.get(x_coor).get(y_coor);
+    }
+}
